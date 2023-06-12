e28074b1e0a6e91830f5eaad4f19f9edd1b048b8
Felipe Espinoza <53890265+fespinozasanchez@users.noreply.github.com>
2023-06-11 20:25:10 -0400

    comments added

--- main.c
+++ main.c
@@ -3,13 +3,32 @@
 #include <string.h>
 #include <jansson.h>
 
+/* `#define MAX_COORDINATES 100` is a preprocessor directive that defines a constant named
+`MAX_COORDINATES` with a value of 100. This constant is used to limit the maximum number of
+coordinates that can be read from the JSON file. */
 #define MAX_COORDINATES 100
 
+/**
+ * The above type defines a structure named "Coordinate" with two double precision variables x and y.
+ * @property {double} x - A double precision floating point number representing the x-coordinate of a
+ * point in a two-dimensional coordinate system.
+ * @property {double} y - The "y" property is a double data type that represents the vertical
+ * coordinate value of a point in a two-dimensional Cartesian coordinate system.
+ */
 typedef struct {
     double x;
     double y;
 } Coordinate;
 
+/**
+ * This function loads a JSON file and returns a JSON object.
+ * 
+ * @param filename a string containing the name of the JSON file to be loaded.
+ * 
+ * @return The function `load_json_file` returns a pointer to a `json_t` object, which represents the
+ * root of a JSON data structure loaded from a file. If there is an error during the loading process,
+ * the function returns `NULL`.
+ */
 json_t *load_json_file(const char *filename) {
     FILE *jsonFile = fopen(filename, "r");
     if (jsonFile == NULL) {
@@ -51,6 +70,17 @@ json_t *load_json_file(const char *filename) {
     return root;
 }
 
+/**
+ * The function extracts coordinates from a JSON array and returns them as a Coordinate struct pointer.
+ * 
+ * @param root A pointer to a JSON object that contains an array of coordinates.
+ * @param numCoordinates A pointer to a size_t variable that will store the number of coordinates
+ * extracted from the JSON array.
+ * 
+ * @return a pointer to an array of Coordinate structs, which represent the coordinates extracted from
+ * a JSON array. It also sets the value of the numCoordinates parameter to the number of coordinates
+ * extracted. If there is an error during the extraction process, the function returns NULL.
+ */
 Coordinate *extract_coordinates(json_t *root, size_t *numCoordinates) {
     if (!json_is_array(root)) {
         printf("El archivo JSON no contiene un array\n");
@@ -95,6 +125,14 @@ Coordinate *extract_coordinates(json_t *root, size_t *numCoordinates) {
     return coordinates;
 }
 
+/**
+ * The function prints out a list of coordinates.
+ * 
+ * @param coordinates A pointer to an array of Coordinate structs.
+ * @param numCoordinates The parameter `numCoordinates` is of type `size_t` and represents the number
+ * of elements in the `coordinates` array. It is used in the `for` loop to iterate over all the
+ * elements of the array.
+ */
 void print_coordinates(Coordinate *coordinates, size_t numCoordinates) {
     printf("Coordenadas:\n");
     for (size_t i = 0; i < numCoordinates; i++) {
