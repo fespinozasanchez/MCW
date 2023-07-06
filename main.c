20c4c40890c99b4c4e8e9a7742405443bb134c24
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2023-07-06 01:51:15 -0400

    doc

--- main.c
+++ main.c
@@ -7,16 +7,47 @@
 #define MAX_COORDINATES 100
 #define GRID_SIZE 2 // nxn grid (2x2)
 
+/**
+ * The above code defines a struct type called Coordinate with two double fields, x and y.
+ * @property {double} x - A double value representing the x-coordinate of a point in a coordinate
+ * system.
+ * @property {double} y - The "y" property is a double data type, which means it can store decimal
+ * numbers. It represents the y-coordinate of a point in a two-dimensional coordinate system.
+ */
 typedef struct {
     double x;
     double y;
 } Coordinate;
 
+/**
+ * The Distance type represents a coordinate and its corresponding distance.
+ * @property {Coordinate} coordinate - The coordinate property is a struct that represents a point in a
+ * two-dimensional space. It typically contains two properties, x and y, which represent the x and y
+ * coordinates of the point.
+ * @property {double} distance - The distance property is a double data type, which represents a
+ * numerical value indicating the distance between two points.
+ */
 typedef struct {
     Coordinate coordinate;
     double distance;
 } Distance;
 
+/**
+ * The GridCell struct represents a cell in a grid, containing coordinates, a centroid, distances, and
+ * a sum of distances.
+ * @property {double} x - A double representing the x-coordinate of the grid cell.
+ * @property {double} y - The y-coordinate of the grid cell.
+ * @property {Coordinate} coordinates - coordinates is an array of pointers to Coordinate objects. It
+ * has a maximum size of MAX_COORDINATES.
+ * @property {int} numCoordinates - The variable "numCoordinates" represents the number of coordinates
+ * stored in the "coordinates" array.
+ * @property {Coordinate} centroid - The centroid property represents the center point of the grid
+ * cell. It is a Coordinate struct that contains the x and y coordinates of the centroid.
+ * @property {Distance} distances - The "distances" property is a pointer to an array of Distance
+ * objects. It is used to store the distances between the GridCell and other GridCells in the grid.
+ * @property {double} sumDistances - The sumDistances property is a double that represents the sum of
+ * all the distances in the GridCell. It is used to keep track of the total distance within the cell.
+ */
 typedef struct {
     double x;
     double y;
@@ -27,6 +58,15 @@ typedef struct {
     double sumDistances; // Sumatoria de las distancias
 } GridCell;
 
+/**
+ * The function `load_json_file` reads a JSON file, loads its contents into a JSON object, and returns
+ * the JSON object.
+ * 
+ * @param filename The filename parameter is a string that represents the name of the JSON file that
+ * you want to load.
+ * 
+ * @return a pointer to a json_t object.
+ */
 json_t *load_json_file(const char *filename) {
     FILE *jsonFile = fopen(filename, "r");
     if (jsonFile == NULL) {
@@ -69,6 +109,16 @@ json_t *load_json_file(const char *filename) {
 }
 
 
+/**
+ * The function `extract_coordinates` takes a JSON array of coordinates and returns an array of
+ * `Coordinate` structs.
+ * 
+ * @param root A pointer to a JSON object that represents an array of coordinates.
+ * @param numCoordinates A pointer to a variable that will store the number of coordinates extracted
+ * from the JSON array.
+ * 
+ * @return a pointer to an array of Coordinate structures.
+ */
 Coordinate *extract_coordinates(json_t *root, size_t *numCoordinates) {
     if (!json_is_array(root)) {
         printf("El archivo JSON no contiene un array\n");
@@ -115,6 +165,15 @@ Coordinate *extract_coordinates(json_t *root, size_t *numCoordinates) {
 
 
 
+/**
+ * The function `print_coordinates` prints the x and y coordinates of an array of `Coordinate`
+ * structures.
+ * 
+ * @param coordinates The "coordinates" parameter is a pointer to an array of Coordinate structures.
+ * @param numCoordinates The parameter `numCoordinates` is the number of coordinates in the
+ * `coordinates` array. It is of type `size_t`, which is an unsigned integer type used for representing
+ * sizes and counts.
+ */
 void print_coordinates(Coordinate *coordinates, size_t numCoordinates) {
     printf("Coordenadas:\n");
     for (size_t i = 0; i < numCoordinates; i++) {
@@ -123,6 +182,16 @@ void print_coordinates(Coordinate *coordinates, size_t numCoordinates) {
 }
 
 
+/**
+ * The function assigns coordinates to cells in a grid based on their position.
+ * 
+ * @param coordinates An array of Coordinate structures, representing the coordinates to be assigned to
+ * the grid.
+ * @param numCoordinates The number of coordinates in the "coordinates" array.
+ * @param grid A pointer to a pointer to a GridCell. This will be used to store the grid.
+ * @param gridSize The gridSize parameter represents the size of the grid. It determines the number of
+ * cells in each row and column of the grid.
+ */
 void assign_coordinates_to_grid(Coordinate *coordinates, size_t numCoordinates, GridCell*** grid, int gridSize) {
     // Calcula el tamaño de la celda de la grid
     double cellSize = 1.0 / gridSize;
@@ -158,6 +227,15 @@ void assign_coordinates_to_grid(Coordinate *coordinates, size_t numCoordinates, 
 
 
 
+/**
+ * The function calculates the centroids of each cell in a grid based on the coordinates stored in each
+ * cell.
+ * 
+ * @param grid The parameter "grid" is a 2D array of GridCell pointers. It represents a grid of cells,
+ * where each cell contains a set of coordinates.
+ * @param gridSize The gridSize parameter represents the size of the grid, which is the number of rows
+ * and columns in the grid.
+ */
 void calculate_cell_centroids(GridCell** grid, int gridSize) {
     for (int i = 0; i < gridSize; i++) {
         for (int j = 0; j < gridSize; j++) {
@@ -178,6 +256,15 @@ void calculate_cell_centroids(GridCell** grid, int gridSize) {
 
 
 
+/**
+ * The function calculates the distances between the centroid of each grid cell and its coordinates.
+ * 
+ * @param grid The grid is a 2D array of GridCell pointers. Each GridCell represents a cell in the grid
+ * and contains information such as the number of coordinates it has and an array of Coordinate
+ * pointers.
+ * @param gridSize The gridSize parameter represents the size of the grid, which is the number of rows
+ * and columns in the grid.
+ */
 void calculate_distances(GridCell** grid, int gridSize) {
     for (int i = 0; i < gridSize; i++) {
         for (int j = 0; j < gridSize; j++) {
@@ -194,6 +281,14 @@ void calculate_distances(GridCell** grid, int gridSize) {
     }
 }
 
+/**
+ * The function heapify is used to maintain the heap property in a binary heap data structure.
+ * 
+ * @param arr The parameter "arr" is an array of type "Distance".
+ * @param n The parameter `n` represents the size of the array `arr`. It indicates the number of
+ * elements in the array that need to be heapified.
+ * @param i The parameter "i" represents the index of the current node in the heap.
+ */
 void heapify(Distance arr[], int n, int i) {
     int largest = i;
     int l = 2 * i + 1;
@@ -214,6 +309,13 @@ void heapify(Distance arr[], int n, int i) {
     }
 }
 
+/**
+ * The function implements the heap sort algorithm to sort an array of Distance objects in ascending
+ * order.
+ * 
+ * @param arr The arr parameter is an array of Distance objects.
+ * @param n The parameter `n` represents the number of elements in the array `arr`.
+ */
 void heap_sort(Distance arr[], int n) {
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
@@ -228,6 +330,15 @@ void heap_sort(Distance arr[], int n) {
 }
 
 
+/**
+ * The function calculates the sum of distances for each cell in a grid.
+ * 
+ * @param grid The parameter "grid" is a pointer to a 2D array of GridCell objects. Each GridCell
+ * object represents a cell in a grid and contains information such as the number of coordinates and an
+ * array of distances.
+ * @param gridSize The gridSize parameter represents the size of the grid, which is the number of rows
+ * and columns in the grid.
+ */
 void calculate_sum_distances(GridCell** grid, int gridSize) {
     for (int i = 0; i < gridSize; i++) {
         for (int j = 0; j < gridSize; j++) {
@@ -242,6 +353,14 @@ void calculate_sum_distances(GridCell** grid, int gridSize) {
     }
 }
 
+/**
+ * The function finds the centroid with the minimum accumulated distance in a grid.
+ * 
+ * @param grid The parameter "grid" is a 2D array of GridCell pointers. Each element in the grid
+ * represents a GridCell object.
+ * @param gridSize The parameter `gridSize` represents the size of the grid, which is the number of
+ * rows and columns in the grid. It indicates the dimensions of the 2D array `grid`.
+ */
 void find_centroid_with_min_distance(GridCell** grid, int gridSize) {
     double minDistance = -1;
     int minDistanceIndexX = -1;
